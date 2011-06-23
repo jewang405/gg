32c1eed615de7fad2eeb49045472b182fa997c75
jewang405 <jewang405@gmail.com>
2011-06-22 20:58:18 -0700

    delete testing line

--- gg.c
+++ gg.c
@@ -7,7 +7,7 @@ def prog_1():
 	n=input("first number: ")
 	m=input("second number: ")
 	#testing
-	print n,m,type(n),type(m)
+#	print n,m,type(n),type(m)
 	#check
 	if type(n)==int  and type(m)==int:
 		return str(n)+"*"+str(m)+"="+str(n*m)
@@ -18,7 +18,7 @@ def prog_2():
 	#get input
 	num=input("enter number: ")
 	#testing
-	print num, type(num)
+#	print num, type(num)
 	#check
 	if type(num)==int:
 		for i in range(1,10):
