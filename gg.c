beb212c976e5b8ff54f87869b2b3c3093954191f
jewang405 <jewang405@gmail.com>
2011-06-22 20:36:39 -0700

    checking integer

--- gg.c
+++ gg.c
@@ -6,10 +6,13 @@ def prog_1():
 	#get input
 	n=input("first number: ")
 	m=input("second number: ")
-	
 	#testing
-	print n
-	print m
+	print n,m,type(n),type(m)
+	#check
+	if type(n)==int  and type(m)==int:
+		print "pass"
+	else:
+		print "error"
 
 #action
 prog_1()
